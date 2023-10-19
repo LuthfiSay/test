5f8852f4e8fae2ce4c6aa80ded3c8a41d2ab6ac5
LuthfiSay <111937633+LuthfiSay@users.noreply.github.com>
2023-10-19 14:46:33 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,18 @@
+pip install pyPMML
+from google.colab import drive
+drive.mount('/content/drive')
+from pypmml import Model
+# Load the PMML model
+model = Model.fromFile('/content/drive/MyDrive/Data Analytics/Revenue Prediction/revenue_prediction_model.pmml')
+# Assuming you have some input data as a dictionary
+input_data = {
+    "Sum(Qty)": 886, #Qty tire michelin yang akan di billing dalam 1 bulan
+    "Sum(EM)": 616, #Qty tire Michelin Earthmover yang akan dibilling dalam 1 bulan
+    "Count(PODate)": 125, #Qty PO Michelin yang akan di billing dalam 1 bulan
+    "Mean(Leadtime)": 295.806, #Target lead time (satuan hari) dari Factory ke CP
+}
 
+# Make prediction
+result = model.predict(input_data)
+
+print(result)
